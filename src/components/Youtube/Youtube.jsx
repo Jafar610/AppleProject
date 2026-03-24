045014c2daa5746c96f56968e21688e2cddf8afc
Jafar610 <jafarmohamed955@gmail.com>
2026-03-24 20:52:25 +0300

    file update

--- src/components/Youtube/Youtube.jsx
+++ src/components/Youtube/Youtube.jsx
@@ -7,8 +7,9 @@ function Youtube() {
             <h1>Latest Video</h1>
             <div className="video">
                 <div className="thumnail">
-                    <video controls>
-                    </video> 
+                    <a href="https://www.youtube.com/watch?v=C6Q4QsszB8Q&list=RDMMC6Q4QsszB8Q&start_radio=1">
+                    <img src="https://i.ytimg.com/vi/C6Q4QsszB8Q/hqdefault.jpg?s…iBTKCkwDw==&rs=AOn4CLAedY0mtohnpTZ8n3kk4QFSOGy6bg" alt="" />
+                    </a>
                 </div>
             </div>
         </div>
